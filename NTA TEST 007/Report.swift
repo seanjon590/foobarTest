af90479b06b1bec44f5d4870f0b90ef684caf2e2
DaKrizzly <krizzly@krizzlys-MacBnop-Anop.local>
2017-03-06 11:37:58 -0500

    Adding skeleton file for report object.

---
+++ NTA TEST 007/Report.swift
@@ -0,0 +1,14 @@
+//
+//  Report.swift
+//  NTA TEST 007
+//
+//  Created by krizzly on 3/6/17.
+//  Copyright © 2017 krizzly. All rights reserved.
+//
+
+import Foundation
+class Report
+{
+    // Class should contain arrays for data values. consider contraints on size of reports. 
+    // Don't want to clog up server/iphone memory 
+}
